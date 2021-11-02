7c03b7bad4af00a2f049011ffb6c92e3b54e825c
Daniel Calvin <arockne2003@yahoo.com>
2021-11-02 13:11:55 -0700

    Add create many potion items connected to potion_shop

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@ armor_smith = Shop.create(name: 'Armor')
 tools = Shop.create(name: 'Tools & Such')
 
 #potions
+Item.create(name: 'Healing Potion', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name:'Greater Strength', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Black Sap', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Lesser Strength', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Invisiblity', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Sleep', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Greater Speed', type: 'potion', pounds: 1, cost: rand(1..50), shop_id: potion_shop.id, desc: Faker::Lorem.sentence)
+Item.create(name: 'Growth', type:, pounds: 1, cost: rand(1..50), shop_id:, desc: Faker::Lorem.sentence)
 
 #weapons
 
