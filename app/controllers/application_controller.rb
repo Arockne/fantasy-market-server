b50cd13c5bc32ef1163bcb3d83cd63016126c8fc
Daniel Calvin <arockne2003@yahoo.com>
2021-11-02 17:13:11 -0700

    Add http get route to get shops with items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
     shops.to_json
   end
 
+  get '/shops/items' do
+    shops = Shop.all
+    shops.to_json(include: {
+      items: { 
+        only: [
+          :id, 
+          :name, 
+          :desc, 
+          :pounds, 
+          :cost, 
+          :category, 
+          :created_at
+        ]
+      }
+    })
+  end
+
 end
