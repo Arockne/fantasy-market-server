65895cef05867be0229e82968c2c5bf83bd5b513
Daniel Calvin <arockne2003@yahoo.com>
2021-11-06 15:44:39 -0700

    Abstract away update and creation of items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,25 @@
 class Item < ActiveRecord::Base
   belongs_to :shop
+
+  def self.create_with_params(params)
+    self.create(
+      name: params[:name],
+      desc: params[:desc],
+      pounds: params[:pounds],
+      cost: params[:cost],
+      category: params[:category],
+      shop_id: params[:shop_id]
+    )  
+  end
+
+  def update_with_params(params)
+    self.update(
+      name: params[:name],
+      desc: params[:desc],
+      pounds: params[:pounds],
+      cost: params[:cost],
+      category: params[:category],
+      shop_id: params[:shop_id]
+    )
+  end
 end
