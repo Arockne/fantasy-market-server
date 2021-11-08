60f08c62dfb4f31fe462d073afc587861bd802cd
Daniel Calvin <arockne2003@yahoo.com>
2021-11-08 13:57:27 -0700

    Remove create_with_params

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :shop
 
-  def self.create_with_params(params)
-    self.create(
-      name: params[:name],
-      desc: params[:desc],
-      pounds: params[:pounds],
-      cost: params[:cost],
-      category: params[:category],
-      shop_id: params[:shop_id]
-    )  
-  end
-
   def update_with_params(params)
     self.update(
       name: params[:name],
@@ -22,4 +11,5 @@ class Item < ActiveRecord::Base
       shop_id: params[:shop_id]
     )
   end
+
 end
